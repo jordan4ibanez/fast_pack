7f29934a842542cd20897982692f7841f7683c6d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-10 03:31:54 -0400

    Add pixel overshoot for OpenGL

--- source/fast_pack/texture_packer.d
+++ source/fast_pack/texture_packer.d
@@ -71,8 +71,9 @@ struct TexturePacker {
         return GLRectDouble(
             cast(double) AABB.x / cast(double) width,
             cast(double) AABB.y / cast(double) height,
-            (cast(double) AABB.x + cast(double) AABB.width) / cast(double) width,
-            (cast(double) AABB.y + cast(double) AABB.height) / cast(double) height            
+            // Needs to overhang the pixel at the base of the next, so + 1
+            (cast(double) AABB.x + cast(double) AABB.width + 1) / cast(double) width,
+            (cast(double) AABB.y + cast(double) AABB.height + 1) / cast(double) height            
         );
     }
 
@@ -82,8 +83,9 @@ struct TexturePacker {
         return GLRectFloat(
             cast(float) AABB.x / cast(float) width,
             cast(float) AABB.y / cast(float) height,
-            (cast(float) AABB.x + cast(float) AABB.width) / cast(float) width,
-            (cast(float) AABB.y + cast(float) AABB.height) / cast(float) height            
+            // Needs to overhang the pixel at the base of the next, so + 1
+            (cast(float) AABB.x + cast(float) AABB.width + 1) / cast(float) width,
+            (cast(float) AABB.y + cast(float) AABB.height + 1) / cast(float) height            
         );
     }
 
