7b8da62636e765239a7e1ba4b6ce17678c0945b7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-10 02:01:59 -0400

    Update app.d

--- source/app.d
+++ source/app.d
@@ -7,19 +7,11 @@ void main() {
     // In this example I am allocating the texture packer onto the heap to clear out the stack
     // *new is optional
 	TexturePacker packer = *new TexturePacker();
-
     for (int i = 1; i <= 10; i++) {
         packer.pack("blah" ~ to!string(i), "assets/" ~ to!string(i) ~ ".png");
     }
+    packer.saveToFile("rawPack.png");
 
 
-    // writeln("packer width = ", packer.getWidth());
-    // writeln("packer height = ", packer.getHeight());
 
-    // writeln(packer.getPixel(0,0));
-
-    packer.saveToFile("test.png");
-
-    // Only use the best debug info when testing 
-    // writeln("it worked");
 }
