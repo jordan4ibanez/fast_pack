0ff03219070b6adffcece08afe594a5d641f8c08
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-09 21:03:48 -0400

    Add more debug testing

--- source/app.d
+++ source/app.d
@@ -1,6 +1,14 @@
 import std.stdio;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import texture_packer;
+
+void main() {
+    // In this example I am allocating the texture packer onto the heap to clear out the stack
+    // *new is optional
+	TexturePacker packer = *new TexturePacker();
+
+    packer.uploadTexture("assets/5.png");
+
+    // Only use the best debug info when testing 
+    writeln("it worked");
 }
