dd6ed84ca74246150198920a7e4f4e468df4d611
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-09 20:56:25 -0400

    Documentation, new function

--- source/texture_packer.d
+++ source/texture_packer.d
@@ -5,14 +5,33 @@ import image;
 import resources.rect;
 import resources.texture_packer_config;
 
+/*
+ * The texture packer structure, can also be allocated to heap via:
+ * TexturePacker blah = *new TexturePacker();
+ */
 struct TexturePacker {
 
+    // The configuration of the texture packer
     TexturePackerConfig config = *new TexturePackerConfig();
 
-    Rectangle[string] collisionBoxes;
+    // This holds the collision boxes of the textures
+    Rect[string] collisionBoxes;
+
+    // This holds the actual texture data
     TrueColorImage[string] textures;
 
+    /*
+     * A constructor with a predefined configuration
+     */
     this(TexturePackerConfig config) {
         this.config = config;
+    }
+
+    /*
+     * Uploads a texture into the associative arrays of the texture packer.
+     * This allows game developers to handle a lot less boilerplate
+     */
+    void uploadTexture(string fileLocation) {
+        
     }
 }
