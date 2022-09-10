df6c98da064e2a893f9d3cdb5c9e956d015a7f3b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-10 01:33:13 -0400

    Fix mistakes & add constructor

--- source/resources/rect.d
+++ source/resources/rect.d
@@ -9,9 +9,16 @@ struct Rect {
     uint width = 0;
     uint height = 0;
 
+    this(uint x, uint y, uint width, uint height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
     // Inverse AABB point check, best case: 1 cpu cycle, worst case: 4 cpu cycles
     bool containsPoint(uint x, uint y) {
-        return !(x < this.x || x > this.x + this.width || y < this.y || y > this.y + this.height);
+        return !(x < this.x || x > this.x + this.width - 1 || y < this.y || y > this.y + this.height - 1);
     }
 
     // Check if a point is on the texture's edge
@@ -21,9 +28,9 @@ struct Rect {
 
     // Inverse AABB check, best cast: 1 cpu cycle, worst cast: 4 cpu cycles
     bool collides(Rect AABB) {
-        return !(AABB.x + AABB.width < this.x ||
+        return !(AABB.x + AABB.width - 1 < this.x ||
                  AABB.x > this.x + this.width ||
-                 AABB.y + AABB.height < this.y ||
+                 AABB.y + AABB.height - 1 < this.y ||
                  AABB.y > this.y + this.height );
     }
 }
