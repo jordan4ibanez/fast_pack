7071b16e33a56a5bb040b4b1fe64237f484c34f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-09 20:39:28 -0400

    Update texture_packer.d

--- source/texture_packer.d
+++ source/texture_packer.d
@@ -1,7 +1,15 @@
 module texture_packer;
 
 import image;
+import resources.rectangle;
 
 struct TexturePacker {
+    Rectangle[string] collisionBoxes;
+    TrueColorImage[string] textures;
+
+    uint border = 0;
+    Color borderColor;
+
+
 
 }
