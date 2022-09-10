989e159b2ef8b4efd7f6ded116d7e1ff607c3a32
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-10 01:04:01 -0400

    Fix up main()

--- source/app.d
+++ source/app.d
@@ -1,15 +1,24 @@
 import std.stdio;
 
 import texture_packer;
+import std.conv: to;
 
 void main() {
     // In this example I am allocating the texture packer onto the heap to clear out the stack
     // *new is optional
 	TexturePacker packer = *new TexturePacker();
 
-    packer.pack("blah", "assets/5.png");
+    for (int i = 1; i <= 10; i++) {
+        packer.pack("blah" ~ to!string(i), "assets/" ~ to!string(i) ~ ".png");
+    }
 
-    packer.debugIt("blah");
+
+    writeln("packer width = ", packer.getWidth());
+    writeln("packer height = ", packer.getHeight());
+
+    writeln(packer.getPixel(0,0));
+
+    packer.saveToFile("test.png");
 
     // Only use the best debug info when testing 
     writeln("it worked");
