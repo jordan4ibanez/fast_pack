cbfbd78b863f252cc892160ae602a38a3a49dc6f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-10 18:35:37 -0500

    This needs to be turned into a full texture packer now

--- source/app.d
+++ source/app.d
@@ -11,21 +11,6 @@ void main() {
     auto rnd = Random(unpredictableSeed());
     TexturePacker packer = TexturePacker();
 
-    foreach (i; 0 .. 400_000) {
-        packer.pack(uniform(20, 40, rnd), uniform(20, 40, rnd));
-    }
-
-    foreach (i; 0 .. 40_000) {
-        packer.pack(uniform(5, 10, rnd), uniform(5, 10, rnd));
-    }
-
-    foreach (i; 0 .. 20_000) {
-        packer.pack(uniform(200, 400, rnd), uniform(200, 400, rnd));
-    }
-
-    packer.pack(100, 110);
-    packer.pack(100, 100);
-    packer.pack(100, 90);
 
     packer.finalize();
 
