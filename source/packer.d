8afbd3aab916c3d97d5fa1a4789b947c21e709af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-11 20:10:23 -0500

    Update packer.d

--- source/packer.d
+++ source/packer.d
@@ -251,6 +251,8 @@ unittest {
     TexturePacker packer = TexturePacker(10);
 
     // Only works with PNG for now.
+
+    //? This is 10_000 textures.
     foreach (j; 0 .. 100) {
         foreach (uint i; 0 .. 10) {
             packer.pack(to!string(i) ~ to!string(j), "assets/" ~ to!string(i + 1) ~ ".png");
