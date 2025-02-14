4132579deb977d7eab5f5245e51e120d7f8c2226
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-14 03:13:40 -0500

    Enable unit tests

--- source/fast_pack.d
+++ source/fast_pack.d
@@ -662,6 +662,111 @@ unittest {
 
     writeln("took: ", sw.peek.total!"msecs", "ms");
 
+    //? Basically, these tests should look the same for all outputs.
+
+    writeln("=== BEGIN OUTPUT STYLE ===");
+
+    // This is to make sure downcasting in getPositionCustom works.
+    struct RectangleTestFloat {
+        float x = 0;
+        float y = 0;
+        float w = 0;
+        float h = 0;
+    }
+
+    RectangleTestFloat test1 = packer.getRectangle!RectangleTestFloat("1");
+    writeln(test1);
+
+    struct RectangleTestDouble {
+        float x = 0;
+        float y = 0;
+        float w = 0;
+        float h = 0;
+    }
+
+    RectangleTestDouble test2 = packer.getRectangle!RectangleTestDouble("1");
+    writeln(test2);
+
+    //? This should never compile.
+    // struct RectangleTestWrong {
+    //     int x = 0;
+    //     float y = 0;
+    //     float w = 0;
+    //     float h = 0;
+    // }
+    // RectangleTestWrong test3 = packer.getRectangle!RectangleTestWrong("1");
+    // writeln(test3);
+
+    struct TestVec2Float {
+        float x = 0;
+        float y = 0;
+    }
+
+    TexturePoints!TestVec2Float test3 = packer.getTexturePoints!TestVec2Float("1");
+    writeln(test3);
+
+    struct TestVec2Double {
+        double x = 0;
+        double y = 0;
+    }
+
+    TexturePoints!TestVec2Double test4 = packer.getTexturePoints!TestVec2Double("1");
+    writeln(test4);
+
+    //? This should never compile.
+    // struct TestVec2Wrong {
+    //     int x = 0;
+    //     double y = 0;
+    // }
+    // TexturePoints!TestVec2Wrong test5 = packer.getTexturePoints!TestVec2Wrong("1");
+    // writeln(test5);
+
+    writeln("=== BEGIN REFERENCE STYLE ===");
+
+    RectangleTestFloat test5;
+    packer.getRectangle("1", test5);
+    writeln(test5);
+
+    RectangleTestDouble test6;
+    packer.getRectangle("1", test6);
+    writeln(test6);
+
+    //? This should never compile.
+    // RectangleTestWrong test7;
+    // packer.getRectangle("1", test7);
+    // writeln(test7);
+
+    TexturePoints!TestVec2Float test7;
+    packer.getTexturePoints!TestVec2Float("1", test7);
+    writeln(test7);
+
+    TexturePoints!TestVec2Double test8;
+    packer.getTexturePoints!TestVec2Double("1", test8);
+    writeln(test8);
+
+    // ? This should never compile.
+    // TexturePoints!TestVec2Wrong test9;
+    // packer.getRectangle("1", test9);
+    // writeln(test9);
+
+    writeln("=== BEGIN GET RECTANGLE ===");
+
+    foreach (uint i; 0 .. 10) {
+        writeln(packer.getRectangle!RectangleTestFloat(to!string(i)));
+        writeln(packer.getRectangle!RectangleTestDouble(to!string(i)));
+    }
+
+    writeln("=== BEGIN GET TEXTURE POINTS ===");
+
+    foreach (uint i; 0 .. 10) {
+        writeln(packer.getTexturePoints!TestVec2Float(to!string(i)));
+        writeln(packer.getTexturePoints!TestVec2Double(to!string(i)));
+    }
+
+    //     packer.flushToMemory();
+
+    ////? TESTING EXTRACTION
+
     struct RectangleTestIntegral {
         int x = 0;
         int y = 0;
@@ -669,8 +774,8 @@ unittest {
         int h = 0;
     }
 
-    RectangleTestIntegral test1 = packer.getRectangleIntegral!RectangleTestIntegral("1");
-    writeln(test1);
+    RectangleTestIntegral test10 = packer.getRectangleIntegral!RectangleTestIntegral("1");
+    writeln(test10);
 
     //? This should never compile.
     // struct RectangleTestIntegralFailure {
@@ -687,8 +792,8 @@ unittest {
         int y = 0;
     }
 
-    TexturePoints!Vec2Integral test3 = packer.getTexturePointsIntegral!Vec2Integral("1");
-    writeln(test3);
+    TexturePoints!Vec2Integral test11 = packer.getTexturePointsIntegral!Vec2Integral("1");
+    writeln(test11);
 
     //? This should never compile.
     // struct Vec2IntegralFailure {
@@ -698,106 +803,4 @@ unittest {
     // TexturePoints!Vec2IntegralFailure test4 = packer.getTexturePointsIntegral!Vec2IntegralFailure("1");
     // writeln(test4);
 
-    //     //? Basically, these tests should look the same for all outputs.
-
-    //     writeln("=== BEGIN OUTPUT STYLE ===");
-
-    //     // This is to make sure downcasting in getPositionCustom works.
-    //     struct RectangleTestFloat {
-    //         float x = 0;
-    //         float y = 0;
-    //         float w = 0;
-    //         float h = 0;
-    //     }
-
-    //     RectangleTestFloat test1 = packer.getRectangle!RectangleTestFloat("1");
-    //     writeln(test1);
-
-    //     struct RectangleTestDouble {
-    //         float x = 0;
-    //         float y = 0;
-    //         float w = 0;
-    //         float h = 0;
-    //     }
-
-    //     RectangleTestDouble test2 = packer.getRectangle!RectangleTestDouble("1");
-    //     writeln(test2);
-
-    //     //? This should never compile.
-    //     // struct RectangleTestWrong {
-    //     //     int x = 0;
-    //     //     float y = 0;
-    //     //     float w = 0;
-    //     //     float h = 0;
-    //     // }
-    //     // RectangleTestWrong test3 = packer.getRectangle!RectangleTestWrong("1");
-    //     // writeln(test3);
-
-    //     struct TestVec2Float {
-    //         float x = 0;
-    //         float y = 0;
-    //     }
-
-    //     TexturePoints!TestVec2Float test3 = packer.getTexturePoints!TestVec2Float("1");
-    //     writeln(test3);
-
-    //     struct TestVec2Double {
-    //         double x = 0;
-    //         double y = 0;
-    //     }
-
-    //     TexturePoints!TestVec2Double test4 = packer.getTexturePoints!TestVec2Double("1");
-    //     writeln(test4);
-
-    //     //? This should never compile.
-    //     // struct TestVec2Wrong {
-    //     //     int x = 0;
-    //     //     double y = 0;
-    //     // }
-    //     // TexturePoints!TestVec2Wrong test5 = packer.getTexturePoints!TestVec2Wrong("1");
-    //     // writeln(test5);
-
-    //     writeln("=== BEGIN REFERENCE STYLE ===");
-
-    //     RectangleTestFloat test5;
-    //     packer.getRectangle("1", test5);
-    //     writeln(test5);
-
-    //     RectangleTestDouble test6;
-    //     packer.getRectangle("1", test6);
-    //     writeln(test6);
-
-    //     //? This should never compile.
-    //     // RectangleTestWrong test7;
-    //     // packer.getRectangle("1", test7);
-    //     // writeln(test7);
-
-    //     TexturePoints!TestVec2Float test7;
-    //     packer.getTexturePoints!TestVec2Float("1", test7);
-    //     writeln(test7);
-
-    //     TexturePoints!TestVec2Double test8;
-    //     packer.getTexturePoints!TestVec2Double("1", test8);
-    //     writeln(test8);
-
-    //     // ? This should never compile.
-    //     // TexturePoints!TestVec2Wrong test9;
-    //     // packer.getRectangle("1", test9);
-    //     // writeln(test9);
-
-    //     writeln("=== BEGIN GET RECTANGLE ===");
-
-    //     foreach (uint i; 0 .. 10) {
-    //         writeln(packer.getRectangle!RectangleTestFloat(to!string(i)));
-    //         writeln(packer.getRectangle!RectangleTestDouble(to!string(i)));
-    //     }
-
-    //     writeln("=== BEGIN GET TEXTURE POINTS ===");
-
-    //     foreach (uint i; 0 .. 10) {
-    //         writeln(packer.getTexturePoints!TestVec2Float(to!string(i)));
-    //         writeln(packer.getTexturePoints!TestVec2Double(to!string(i)));
-    //     }
-
-    //     packer.flushToMemory();
 }
